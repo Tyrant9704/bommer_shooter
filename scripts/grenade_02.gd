6b595c1458d1ac39d3edb657ac88ee25eafad321
mikcodelab <michal.kulawik@gmail.com>
2023-06-05 07:18:04 +0200

    dmg fix, light fix

--- scripts/grenade_02.gd
+++ scripts/grenade_02.gd
@@ -1,7 +1,7 @@
 extends RigidBody3D
 
 
-const GRENADE_DAMAGE = 100
+const GRENADE_DAMAGE = 20
 
 const GRENADE_TIME = 2
 var grenade_timer = 0
@@ -11,6 +11,9 @@ var explosion_wait_timer = 0
 
 #duration of discharge (emitting)
 const DURATION = 4.0
+
+const MAX_LIGHT_ENERGY = 30
+const MAX_RANGE = 8
 
 @onready var rigid_shape = $CollisionShape3D
 @onready var grenade_mesh = $MeshInstance3D
@@ -35,23 +38,23 @@ func _process(delta):
 			emission_particles.emitting = true
 			distortion.visible = true
 			
-			explosion_light.light_energy = 20.0
 			explosion_light.light_indirect_energy = 10.0
 			
-			#it has to be rigid body
-			#grenade_mesh.visible = false
-			#rigid_shape.disabled = true
-			
 			explosion_sfx.play()
+					
+		if explosion_wait_timer < EXPLOSION_WAIT_TIME:
+			explosion_wait_timer += delta
+			
 			var bodies = blast_area.get_overlapping_bodies()
 			for body in bodies:
 				if body.is_in_group('enemy'):
-#					body.apply_central_impulse(Vector3(10, 10, 10))
-					body.health -= GRENADE_DAMAGE
-					
-		
-		if explosion_wait_timer < EXPLOSION_WAIT_TIME:
-			explosion_wait_timer += delta
+					body._health(delta * GRENADE_DAMAGE)
+			# 
+			if explosion_light.light_energy < MAX_LIGHT_ENERGY:
+				explosion_light.light_energy += delta * 20
+				
+			if explosion_light.omni_range < MAX_RANGE:
+				explosion_light.omni_range += delta * 20
 			
 			if explosion_wait_timer >= EXPLOSION_WAIT_TIME:
 				queue_free()
