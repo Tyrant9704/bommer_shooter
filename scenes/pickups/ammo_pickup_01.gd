87d7e97afe7ceb04fe685b1be5c08efc5a657190
mikcodelab <michal.kulawik@gmail.com>
2023-07-01 08:13:40 +0200

    cleanup

--- scenes/pickups/ammo_pickup_01.gd
+++ scenes/pickups/ammo_pickup_01.gd
@@ -9,12 +9,13 @@ enum EAmmoType {standard, heavy, special, sniper}
 @onready var heavy_ammo_box = $heavyAmmoBox
 @onready var special_ammo_box = $specialAmmoBox
 @onready var sniper_ammo_box = $sniperAmmoBox
+@onready var area = $Area3D
 
 
 @export var quantity = 10
 
-@onready var audio = $AudioStreamPlayer3D
-@onready var audio_2 = $AudioStreamPlayer
+@onready var audio = $AudioStreamPlayer
+
 func _ready():
 	if ammo_type == EAmmoType.standard:
 		standard_ammo_box.visible = true
@@ -28,18 +29,13 @@ func _ready():
 
 func _on_area_3d_body_entered(body):
 	if body.has_method('add_ammo'):
+		area.disconnect("body_entered", _on_area_3d_body_entered)
 		body.add_ammo(ammo_type, quantity)
-#		audio.play()
-		audio_2.play()
+		audio.play()
 		standard_ammo_box.visible = false
 		heavy_ammo_box.visible = false
 		special_ammo_box.visible = false
 		sniper_ammo_box.visible = false
-
-
-
-func _on_audio_stream_player_3d_finished():
-		queue_free()		
 
 
 func _on_audio_stream_player_finished():
